1ad505e0ea3e996767a7756160e5a110c611ffed
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2022-08-18 15:14:24 +0800

    fix unpack vars #9

--- behavior3/behavior_node.lua
+++ behavior3/behavior_node.lua
@@ -39,7 +39,11 @@ function mt:run(env)
         vars[i] = env:get_var(var_name)
     end
     local func = assert(process[self.name].run, self.name)
-    vars = table.pack(func(self, env, table.unpack(vars, 1, #self.data.input)))
+    if self.data.input then
+        vars = table.pack(func(self, env, table.unpack(vars, 1, #self.data.input)))
+    else
+        vars = table.pack(func(self, env, table.unpack(vars)))
+    end
     local ret = vars[1]
     assert(ret, self.info)
     if ret ~= bret.RUNNING then
