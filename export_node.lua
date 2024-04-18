f21b313426d81af2ffc39d8ac441093aa04b29c6
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2024-04-18 10:14:28 +0800

    update editor workspace

--- export_node.lua
+++ export_node.lua
@@ -4,33 +4,33 @@ local json = require "json"
 local process = require "example.process"
 
 local nodes = {}
-for k, v in pairs(process) do  
-    if v.args then             
-        for i, vv in pairs(v.args) do   
-            v.args[i] = {      
-                name = vv[1],  
-                type = vv[2],  
-                desc = vv[3],  
+for k, v in pairs(process) do
+    if v.args then
+        for i, vv in pairs(v.args) do
+            v.args[i] = {
+                name = vv[1],
+                type = vv[2],
+                desc = vv[3],
             }
         end
     end
-    
-    local doc = v.doc          
-    if type(doc) == "string" then   
+
+    local doc = v.doc
+    if type(doc) == "string" then
         doc = string.gsub(doc, "^([ ]+", "")
         doc = string.gsub(doc, "\n([ ]+", "\n")
     end
-    
+
     local node = {
-        name   = v.name,       
-        type   = v.type,       
-        desc   = v.desc,       
-        args   = v.args,       
-        input  = v.input,      
-        output = v.output,     
-        doc    = doc,          
+        name   = v.name,
+        type   = v.type,
+        desc   = v.desc,
+        args   = v.args,
+        input  = v.input,
+        output = v.output,
+        doc    = doc,
     }
-    table.insert(nodes, node)  
+    table.insert(nodes, node)
 end
 
 table.sort(nodes, function(a, b)
@@ -40,7 +40,7 @@ end)
 local str = json.encode(nodes)
 print(str)
 
-local path = "workspace/node-config.json"
+local path = "workspace/node-config.b3-setting"
 local file = io.open(path, "w")
 file:write(str)
 file:close()
