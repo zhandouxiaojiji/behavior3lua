052bf462e592c7374bf1a1f7a95d032a7eb0fe8b
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2024-07-17 10:55:24 +0800

    fix sample_process

--- behavior3/sample_process.lua
+++ behavior3/sample_process.lua
@@ -1,10 +1,10 @@
 return {
   -- 复合节点
-  IfElse          = require "behavior3.nodes.composites.ifelse",
-  Parallel        = require "behavior3.nodes.composites.parallel",
-  Selector        = require "behavior3.nodes.composites.selector",
-  Sequence        = require "behavior3.nodes.composites.sequence",
-  
+  IfElse             = require "behavior3.nodes.composites.ifelse",
+  Parallel           = require "behavior3.nodes.composites.parallel",
+  Selector           = require "behavior3.nodes.composites.selector",
+  Sequence           = require "behavior3.nodes.composites.sequence",
+
   -- 装饰节点
   Once               = require "behavior3.nodes.decorators.once",
   Not                = require "behavior3.nodes.decorators.not",
@@ -13,25 +13,24 @@ return {
   AlwaysSuccess      = require "behavior3.nodes.decorators.always_success",
   RepeatUntilSuccess = require "behavior3.nodes.decorators.repeat_until_success",
   RepeatUntilFailure = require "behavior3.nodes.decorators.repeat_until_fail",
-  RepeatUntilSuccess = require "behavior3.nodes.decorators.repeat_until_success",
 
   -- 条件节点
-  Check           = require "behavior3.nodes.conditions.check",
-  Cmp             = require "behavior3.nodes.conditions.cmp",
-  Includes        = require "behavior3.nodes.conditions.includes",
-  
+  Check              = require "behavior3.nodes.conditions.check",
+  Cmp                = require "behavior3.nodes.conditions.cmp",
+  Includes           = require "behavior3.nodes.conditions.includes",
+
   -- 行为节点
-  Calculate       = require "behavior3.nodes.actions.calculate",
-  Clear           = require "behavior3.nodes.actions.clear",
-  Filter          = require "behavior3.nodes.actions.filter",
-  ForEach         = require "behavior3.nodes.actions.foreach",
-  Index           = require "behavior3.nodes.actions.index",
-  Let             = require "behavior3.nodes.actions.let",
-  Log             = require "behavior3.nodes.actions.log",
-  Loop            = require "behavior3.nodes.actions.loop",
-  Now             = require "behavior3.nodes.actions.now",
-  Push            = require "behavior3.nodes.actions.push",
-  Random          = require "behavior3.nodes.actions.random",
-  RandomIndex     = require "behavior3.nodes.actions.random_index",
-  Wait            = require "behavior3.nodes.actions.wait",
+  Calculate          = require "behavior3.nodes.actions.calculate",
+  Clear              = require "behavior3.nodes.actions.clear",
+  Filter             = require "behavior3.nodes.actions.filter",
+  ForEach            = require "behavior3.nodes.actions.foreach",
+  Index              = require "behavior3.nodes.actions.index",
+  Let                = require "behavior3.nodes.actions.let",
+  Log                = require "behavior3.nodes.actions.log",
+  Loop               = require "behavior3.nodes.actions.loop",
+  Now                = require "behavior3.nodes.actions.now",
+  Push               = require "behavior3.nodes.actions.push",
+  Random             = require "behavior3.nodes.actions.random",
+  RandomIndex        = require "behavior3.nodes.actions.random_index",
+  Wait               = require "behavior3.nodes.actions.wait",
 }
